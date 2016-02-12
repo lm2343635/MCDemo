dc42739e4e79e2003882370305c2780033cd7cbb
lm2343635 <lm2343635@126.com>
2016-02-12 15:39:11 +0900

    Initial Commit

---
+++ MCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCDemo
+//
+//  Created by limeng on 2/12/16.
+//  Copyright © 2016 limeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCDemo/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  MCDemo
+//
+//  Created by limeng on 2/12/16.
+//  Copyright © 2016 limeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
